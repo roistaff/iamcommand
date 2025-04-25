51c70352b48386e6d8cd52918671bcd6ca3f1e5b
roistaff <roistaff1983@gmail.com>
2025-04-25 20:56:30 +0900

    update

--- iamcommand.sh
+++ iamcommand.sh
@@ -2,21 +2,18 @@
 ESC=$(printf '\033')
 echo -n "${ESC}[33mTo computer:${ESC}[m"
 prompt=$(gum input | sed -e "s/\"/\'/g")
-json=$(cat << EOS
-{"messages": [{"role":"system","content":"Tell me the command to do it in terminal. output must be such '$ command' ,and enclose by backticks."},{"role": "user", "content":"{$prompt}"}], "model": "llama3-70b-8192"}
-EOS
-)
-echo $json | jq > $HOME/chat.json
-output=$(curl -X POST "https://api.groq.com/openai/v1/chat/completions" \
+json=$(jq -nc --arg prompt "$prompt" '{messages: [{role: "system",content: "Tell me the command to do it in terminal. output must be only one command,no explain, no markdown"},{role: "user",content: $prompt}],model: "llama3-70b-8192"}')
+response=$(curl -s -X POST "https://api.groq.com/openai/v1/chat/completions" \
   -H "Authorization: Bearer $GROQ_API_KEY" \
-  -s \
   -H "Content-Type: application/json" \
-  -d @$HOME/chat.json > $HOME/answer.json && cat $HOME/answer.json |  jq '.choices[0].message.content')
+  -d "$json")
+output=$(echo $response | jq '.choices[0].message.content')
 if [ "$output" = "null" ]; then
-	echo $(cat $HOME/answer.json | jq '.error.message')
+	echo $(echo "$response" | jq '.error.message')
 else
 ##	output=$(echo $output | sed 's/^.*"\(.*\)".*$/\1/' )
-	command=$(echo $output | rev |  cut -d '$' -f 1 | cut -d '`' -f 2 | rev)
+	#command=$(echo $output | rev |  cut -d '$' -f 1 | cut -d '`' -f 2 | rev)
+	command=$output
 	gum confirm "Run ${command}?"
 	if [ $? = "0" ] ; then
 		eval "$command"
